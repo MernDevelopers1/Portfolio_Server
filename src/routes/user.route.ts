354cbed78e54e1038144d487f83388d5be14dfa4
MernDevelopers1 <MernDeveloper2@gmail.com>
2025-07-03 02:55:07 +0500

    Some little changes in api routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -8,24 +8,19 @@ import {
 } from "../controllers/user.controller";
 import errorHandler from "../middlewares/errorHandler";
 import { validateUserMiddleware } from "../middlewares/auth.middleware";
+import { asyncHandler } from "../utils/AsyncHandler";
 
 const router = express.Router();
 
-// Helper to wrap async route handlers
-const asyncHandler =
-  (fn: any) =>
-  (req: express.Request, res: express.Response, next: express.NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
-
-router.get("/", asyncHandler(getAllUsers), errorHandler);
-router.get("/:id", asyncHandler(getUserById), errorHandler);
+router.get("/users/", asyncHandler(getAllUsers), errorHandler);
+router.get("/users/:id", asyncHandler(getUserById), errorHandler);
 router.post(
-  "/",
+  "/users/",
   validateUserMiddleware,
   asyncHandler(createUser),
   errorHandler
 );
-router.patch("/:id", asyncHandler(updateUser), errorHandler);
-router.delete("/:id", asyncHandler(deleteUser), errorHandler);
+router.patch("/users/:id", asyncHandler(updateUser), errorHandler);
+router.delete("/users/:id", asyncHandler(deleteUser), errorHandler);
 
 export default router;
